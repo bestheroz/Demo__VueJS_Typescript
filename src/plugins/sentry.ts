5757bac5c0629e2ebdfc350a9ecb1b55e7331008
joony <joony@kakaopage.com>
2021-05-27 14:48:52 +0900

    ✨ 센트리 설정 변경

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -1,18 +1,16 @@
-import * as Sentry from "@sentry/browser";
-import { Vue as VueIntegration } from "@sentry/integrations";
+import * as Sentry from "@sentry/vue";
 import { Integrations } from "@sentry/tracing";
 import Vue from "vue";
 
 if (process.env.VUE_APP_SENTRY_DSN) {
   Sentry.init({
+    Vue: Vue,
     dsn: process.env.VUE_APP_SENTRY_DSN,
-    integrations: [
-      new Integrations.BrowserTracing(),
-      new VueIntegration({
-        Vue,
-        tracing: true,
-      }),
-    ],
-    tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0, // Be sure to lower this in production
+    environment: process.env.VUE_ENVIRONMENT,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: process.env.NODE_ENV === "production" ? 1.0 : 1.0, // Be sure to lower this in production
+    tracingOptions: {
+      trackComponents: true,
+    },
   });
 }
