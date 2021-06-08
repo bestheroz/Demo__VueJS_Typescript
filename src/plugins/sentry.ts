5ac02acbaeb15b9bf749e2033d76af82fa205f70
joony <joony@kakaopage.com>
2021-06-08 14:58:20 +0900

    🚀 로컬환경일때에는 sentry 동작하지 않도록 설정

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/vue";
 import { Integrations } from "@sentry/tracing";
 import Vue from "vue";
 
-if (process.env.VUE_APP_SENTRY_DSN) {
+if (process.env.VUE_APP_SENTRY_DSN && process.env.VUE_ENVIRONMENT !== "local") {
   Sentry.init({
     Vue: Vue,
     dsn: process.env.VUE_APP_SENTRY_DSN,
