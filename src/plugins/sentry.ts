a1ba971cc600f741f1173e0cf7d4f019b56e378a
joony <joony@kakaopage.com>
2021-06-15 15:25:35 +0900

    :bug: VUE_APP_ENVIRONMENT

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -2,11 +2,14 @@ import * as Sentry from "@sentry/vue";
 import { Integrations } from "@sentry/tracing";
 import Vue from "vue";
 
-if (process.env.VUE_APP_SENTRY_DSN && process.env.VUE_ENVIRONMENT !== "local") {
+if (
+  process.env.VUE_APP_SENTRY_DSN &&
+  process.env.VUE_APP_ENVIRONMENT !== "local"
+) {
   Sentry.init({
     Vue: Vue,
     dsn: process.env.VUE_APP_SENTRY_DSN,
-    environment: process.env.VUE_ENVIRONMENT,
+    environment: process.env.VUE_APP_ENVIRONMENT,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: process.env.NODE_ENV === "production" ? 1.0 : 1.0, // Be sure to lower this in production
     tracingOptions: {
