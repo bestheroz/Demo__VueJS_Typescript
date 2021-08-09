0b31676efed84dd246a25f62458c1b6362bf39b0
joony <joony@kakaopage.com>
2021-08-09 11:34:28 +0900

    ✨ @sentry/webpack-plugin 추가

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 module.exports = {
   transpileDependencies: ["vuetify"],
   css: {
@@ -24,4 +26,20 @@ module.exports = {
     config.plugins.delete("preload");
     config.plugins.delete("prefetch");
   },
+  configureWebpack:
+    process.env.NODE_ENV === "production"
+      ? {
+          devtool: "source-map",
+          plugins: [
+            new SentryWebpackPlugin({
+              authToken:
+                "d55afb6a392443ddbe98e05d8a079b7d4cceb631027843c5a70489e681d9bb70",
+              org: "bestheroz",
+              project: "demo-vuejs-typescript",
+              include: "./dist",
+              ignore: ["node_modules", "webpack.config.js"],
+            }),
+          ],
+        }
+      : {},
 };
