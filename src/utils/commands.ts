cb294c7f1fb73f2b464af1a7882cc8e61c8d31c4
joony <joony.kim@kakaoent.com>
2021-12-06 11:05:38 +0900

    fix

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -21,6 +21,18 @@ export async function routerPush(path: string): Promise<void> {
   }
 }
 
+export function routerToNewTab(path: string): void {
+  window.open(path, "_blank");
+}
+
+export function routerToNewWindow(path: string): void {
+  window.open(
+    path,
+    "_blank",
+    "location=false,menubar=false,scrollbars=true,status=false,toolbar=false",
+  );
+}
+
 export async function goSignInPage(): Promise<void> {
   await routerReplace("/sign-in");
 }
