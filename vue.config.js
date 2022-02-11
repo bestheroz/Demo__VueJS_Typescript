ad514a4cd20e4567aa456820f84acaa661815f4f
joony <joony.kim@kakaoent.com>
2022-02-11 22:34:10 +0900

    fixxx

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 module.exports = {
   transpileDependencies: ["vuetify"],
   css: {
@@ -17,5 +21,41 @@ module.exports = {
         additionalData: "@import '~@/assets/scss/vuetify/variables';",
       },
     },
+  },
+  productionSourceMap: false,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.devtool = "hidden-source-map";
+      config.plugins = [
+        ...config.plugins, // this is important !
+        new ForkTsCheckerWebpackPlugin({
+          typescript: {
+            diagnosticOptions: {
+              semantic: true,
+              syntactic: true,
+            },
+          },
+        }),
+        process.env.VUE_APP_ENVIRONMENT === "production"
+          ? new SentryWebpackPlugin({
+              authToken:
+                "d55afb6a392443ddbe98e05d8a079b7d4cceb631027843c5a70489e681d9bb70",
+              org: "bestheroz",
+              project: "demo-vuejs-typescript",
+              include: "./dist",
+              ignore: ["node_modules", "webpack.config.js"],
+            })
+          : undefined,
+      ];
+    } else {
+      config.devtool = "eval";
+    }
+    config.output.pathinfo = false;
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule("tsx")
+      .use("ts-loader")
+      .options({ happyPackMode: true, transpileOnly: true });
   },
 };
