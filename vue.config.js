c761b478ed1d2420735a7dacd124a4b1a70fdd6d
joony <joony.kim@kakaoent.com>
2022-02-11 14:24:57 +0900

    fixxx

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const SentryWebpackPlugin = require("@sentry/webpack-plugin");
-
 module.exports = {
   transpileDependencies: ["vuetify"],
   css: {
@@ -21,20 +18,4 @@ module.exports = {
       },
     },
   },
-  configureWebpack:
-    process.env.VUE_APP_ENVIRONMENT !== "local"
-      ? {
-          devtool: "source-map",
-          plugins: [
-            new SentryWebpackPlugin({
-              authToken:
-                "d55afb6a392443ddbe98e05d8a079b7d4cceb631027843c5a70489e681d9bb70",
-              org: "bestheroz",
-              project: "demo-vuejs-typescript",
-              include: "./dist",
-              ignore: ["node_modules", "webpack.config.js"],
-            }),
-          ],
-        }
-      : {},
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 module.exports = {
+  devtool: process.env.NODE_ENV === "production" ? "hidden-source-map" : "eval",
   context: __dirname, // to automatically find tsconfig.json
   entry: "./src/index.ts",
+  output: {
+    pathinfo: false,
+  },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
   },
@@ -15,10 +21,32 @@ module.exports = {
         exclude: /node_modules/,
         options: {
           // disable type checker - we will use it in fork plugin
+          happyPackMode: true,
           transpileOnly: true,
         },
       },
     ],
   },
-  plugins: [new ForkTsCheckerWebpackPlugin()],
+  plugins: [
+    process.env.NODE_ENV === "production"
+      ? new ForkTsCheckerWebpackPlugin({
+          typescript: {
+            diagnosticOptions: {
+              semantic: true,
+              syntactic: true,
+            },
+          },
+        })
+      : undefined,
+    process.env.VUE_APP_ENVIRONMENT === "production"
+      ? new SentryWebpackPlugin({
+          authToken:
+            "d55afb6a392443ddbe98e05d8a079b7d4cceb631027843c5a70489e681d9bb70",
+          org: "bestheroz",
+          project: "demo-vuejs-typescript",
+          include: "./dist",
+          ignore: ["node_modules", "webpack.config.js"],
+        })
+      : undefined,
+  ],
 };
