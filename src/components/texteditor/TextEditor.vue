e64f1b40e66d91c7c1126413f8d67f6b88886eb3
joony <joony.kim@kakaoent.com>
2022-02-21 11:53:04 +0900

    fixxx

--- src/components/texteditor/TextEditor.vue
+++ src/components/texteditor/TextEditor.vue
@@ -5,16 +5,13 @@
       :id="trixId"
       v-model="vModel"
       :placeholder="placeholder"
-      :height="height"
       @trix-attachment-add="handleAttachmentChanges"
-      :style="{ height: height }"
+      :style="{ 'min-height': minHeight }"
     />
   </div>
 </template>
 
 <script lang="ts">
-import { uploadFileApi } from "@/utils/apis";
-import { getImageUrl } from "@/utils/formatter";
 import { computed, defineComponent, onMounted } from "@vue/composition-api";
 import setupVModel from "@/composition/setupVModel";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -30,9 +27,9 @@ export default defineComponent({
     },
     cssId: { type: String, default: "" },
     label: { type: String, default: undefined },
-    folderName: { type: String, default: "uploaded/textEditor" },
-    apiUrl: { type: String, default: "upload/file" },
-    height: { type: String, default: "40vh" },
+    filePath: { type: String, default: "text-editor" },
+    apiUrl: { type: String, default: "s3-files/" },
+    minHeight: { type: String, default: "40vh" },
     placeholder: { type: String, default: "Please enter text." },
   },
   setup(props, { emit }) {
@@ -41,26 +38,6 @@ export default defineComponent({
       trixId: computed(() => `trixId-${props.cssId}`),
     };
     const methods = {
-      async handleAttachmentChanges(event: any) {
-        // 1. get file object
-        const file = event.attachment.file;
-
-        // 2. upload file to remote server with FormData
-        const formData = new FormData();
-        formData.append("folderName", props.folderName);
-        formData.append("file", file);
-        await uploadFileApi(props.apiUrl, formData);
-        const result = await uploadFileApi(props.apiUrl, formData);
-
-        // 3. if upload success, set back the attachment's URL attribute
-        // @param object data from remote server response data after upload.
-        let attributes = {
-          url: getImageUrl(result.data),
-          href: getImageUrl(result.data),
-        };
-        event.attachment.setAttributes(attributes);
-      },
-
       validateText: (): boolean => {
         // html 태그 삭제 정규식
         const extractTextPattern = /(<([^>]+)>)/gi;
@@ -72,7 +49,7 @@ export default defineComponent({
         `#${computes.trixId.value}>trix-editor`,
       );
       if (selector) {
-        selector.style.height = props.height;
+        selector.style.minHeight = props.minHeight;
       }
     });
     return { ...vModel, ...computes, ...methods };
@@ -83,5 +60,22 @@ export default defineComponent({
 <style lang="scss">
 button.trix-button--icon {
   background-color: #a6a6a6;
+}
+.trix-content {
+  .attachment--preview {
+    text-align: left;
+  }
+  .attachment__caption {
+    text-align: left;
+    .attachment__caption-editor {
+      text-align: left;
+    }
+  }
+  .attachment__toolbar {
+    text-align: left;
+  }
+  .attachment__metadata {
+    position: initial;
+  }
 }
 </style>
